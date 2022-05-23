5dd93537a97d31705b03c55f93a9d2bcdbb7d22a
Makkapakka <1961605387@qq.com>
2022-05-23 14:39:57 +0800

    初次提交前端

---
+++ src/router/index.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import AppIndex from '../components/home/AppIndex'
+import Login from '../components/Login'
+import Home from '../components/Home'
+import LibraryIndex from '../components/library/LibraryIndex'
+import Register from '../components/Register'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+      redirect: '/home'
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }
+  ]
+})
